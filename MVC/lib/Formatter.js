491cfa36b386282bf995583bc9695fa448b27a65
OlgaMayevski <46324933+OlgaMayevski@users.noreply.github.com>
2019-02-18 17:08:01 +0300

    sapUI5
    test

---
+++ MVC/lib/Formatter.js
@@ -0,0 +1,18 @@
+﻿sap.ui.define([], function () {
+    return {
+        capitalizeFirstLetter: function (sValue) {
+            return sValue.charAt(0).toUpperCase() + sValue.slice(1);
+        }
+    };
+});
+
+/lib/MessageManager.js
+    sap.ui.define(["sap/m/MessageBox", "sap/hello/lib/Formatter"], function (MessageBox, Formatter) {
+        return {
+            reportSuccess: function (sMsg, sTitle) {
+                MessageBox.show(Formatter.capitalizeFirstLetter(sMsg), {
+                    title: Formatter.capitalizeFirstLetter(sTitle)
+                });
+            }
+        };
+    });
---
+++ MVC/lib/MessageManager.js
@@ -0,0 +1,9 @@
+﻿sap.ui.define(["sap/m/MessageBox", "sap/hello/lib/Formatter"], function (MessageBox, Formatter) {
+    return {
+        reportSuccess: function (sMsg, sTitle) {
+            MessageBox.show(Formatter.capitalizeFirstLetter(sMsg), {
+                title: Formatter.capitalizeFirstLetter(sTitle)
+            });
+        }
+    };
+});
